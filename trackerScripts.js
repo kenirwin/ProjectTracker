16e0c421d669f5ea229211e9ba05ce3a4d573848
Ken Irwin <kirwin@wittenberg.edu>
2017-03-07 17:28:05 -0500

    edit can add new Item to the UI, but not Series or Section

--- trackerScripts.js
+++ trackerScripts.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
 	    var rowContent = '<li class="add">+ New Item</li>';
 	}
 	$(this).append(rowContent);
-    });
+    })
     $('body').append('<ul class="section add"><header>+ New Section</header></ul>');
+
+    $('.add').click(function(e) {
+	this.addLink = $(this);
+	this.addLink.hide();
+	data = {"link": this.addLink }
+	var textType = this.addLink.text().substr(1); //drop the plus sign
+	var form = $('<form></form>').append('<input id="new_item" type="text" placeholder="Enter'+textType+'"/><input type="submit" />');
+	form.on('submit', data, function(e) {
+	    e.preventDefault();
+	    $(this).hide();
+	    var newItem = form.find('#new_item').val();
+	    $(this).before('<li class="undone">'+newItem+'</li>');
+	    data.link.show();
+	});
+	form = form.wrap('<li></li>');
+	this.addLink.before(form);
+	
+	
+	
+    });
+    
+
 });
