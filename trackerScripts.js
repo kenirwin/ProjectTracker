75bf4b0e6e960e71b9f73c1bd3bbe69462810a93
Ken Irwin <kirwin@wittenberg.edu>
2017-03-14 16:40:01 -0400

    cleaned up old Editor comments, console.log etc

--- trackerScripts.js
+++ trackerScripts.js
@@ -4,7 +4,6 @@ var Editor = {
 	init: function () {
 	    Editor.i = 0;
 	    $('ul').each( function() {
-	    	//console.log( $(this) );
 	    	Editor.addNewLink( $(this) );
 		});
 		Editor.addNewLink( $('body') );
@@ -15,8 +14,6 @@ var Editor = {
     },
 	addNewLink: function(parent) {
 		var i = Editor.nextI();
-		console.log("you clicked on a");
-		console.log(parent.prop("tagName").toLowerCase());
 		var parentClass = parent.attr('class');
 		if (parentClass === "section") {
 	    	var rowContent = '<li class="add add-series" id="item-'+i+'"><header class="add-series-inner" data-childOf="item-'+i+'">+ New Series</header></li>';
@@ -46,13 +43,10 @@ var Editor = {
 			var entryType = "item";
 		}
 		addLink.hide();
-		//series = addLink.closest('.series');
 		section = addLink.closest('.section');
 		data = { "link": addLink, "section": section }
-		var textType = addLink.text().substr(1); //drop the plus sign
-		//var form = $('<form></form>').append('<input id="new_item" type="text" placeholder="Enter'+textType+'"/><input type="hidden" id="entryType" value="'+textType+'"><input type="submit" />');		
+		var textType = addLink.text().substr(1); //drop the plus sign	
 		var form = $('<form id="item-'+i+'"></form>').append('<input id="new_item" type="text" placeholder="Enter '+entryType+'" /><input type="hidden" id="entryType" value="'+entryType+'"><input type="submit" />');
-		//form = $(form).wrap('<li></li>');
 		addLink.before(form);
 		$('#new_item').focus();
 		$('form').on('submit', data, function (event) { 
@@ -66,27 +60,21 @@ var Editor = {
 		var i = Editor.nextI();
 		var newItem = $('#new_item');
 		var newItemText = newItem.val();
-		var section = data.section; 
-		//var series = data.series;
+		var section = data.section; //never used?
 		form = $(event.target);
 		if ( $('#entryType').val() === "series" ) {
-    		console.log('doing: parent is section');
-    		$(form).before('<li id="item-'+i+'" class="added"><header data-childOf="'+i+'">'+newItemText+'</header></li>');
-    		Editor.addNewLink($('#item-'+i+" header"));
-    		//TO DO: trigger creation of new item-level add-link
+    		$(form).before('<li id="item-'+i+'" class="added"><header data-childOf="'+i+'">'+newItemText+'</header><ul class="series" id="ul-for-'+i+'"></li>');
+    		Editor.addNewLink($('#ul-for-'+i));
     	}
     	else if ($('#entryType').val() === "section") {
     		$(form).before('<ul class="section" id="item-'+i+'"><header>'+newItemText+'</header></ul>');
     		$('.section').last().trigger('addNewLink');
     		Editor.addNewLink($('#item-'+i));
-    		//TO DO: trigger creation of new series-level add-link
     	}
     	else { 
 	    	$(form).before('<li class="undone" id="item-'+i+'">'+newItemText+'</li>');
 		}
 		$(form).remove();
-//doesnt work		data.link.parent().find('.add').show();	
-
 	},
 	displayNew: function() {
 	
